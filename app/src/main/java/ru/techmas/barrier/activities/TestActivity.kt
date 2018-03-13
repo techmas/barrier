9d18ec6467329a2bf1e643ed46c22d6f84573e5b
Natalia Antipieva <n.antipieva@gmail.com>
2018-03-13 11:56:44 +0100

    Итемка шлагбаум

--- app/src/main/java/ru/techmas/barrier/activities/TestActivity.kt
+++ app/src/main/java/ru/techmas/barrier/activities/TestActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
